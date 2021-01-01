ca592b59bb0160535ba5eb958b87066a20804ddd
Sundar Shahi Thakuri <shahithakurisundar@gmail.com>
2021-01-01 21:20:36 +0545

    remove

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -22,7 +22,7 @@ const Contact = () => {
   } = useForm();
 
   const onSubmit = (data: any) => {
-    fetch('/?no-cache=1', {
+    fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
